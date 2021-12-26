3e7bce58f7bc001fcd5102074b3334667ae0ef8a
Cihan <cihangonen35@gmail.com>
2021-12-26 22:34:48 +0300

    cv updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CV from "./CihanGonen-React,Node.pdf";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        id="pdf-js-viewer"
+        src={`web/viewer.html?file=${CV}`}
+        title="webviewer"
+        width="100%"
+        height="100vw"
+      ></iframe>
     </div>
   );
 }
